c18697939174dc26b588d1b0e031f96bc6009a87
LUCIANO CAMPOS DE LIMA JUNIOR <20161D13GR0210@LABS.GARANHUNS.IFPE>
2018-08-16 21:35:03 -0300

    comecando a fazer a versar 2.0

---
+++ app/src/main/java/br/com/gustech/android/geoquiz/Question.java
@@ -0,0 +1,28 @@
+package br.com.gustech.android.geoquiz;
+
+public class Question {
+
+    private int mQuestaoID;
+    private boolean mRespostaCorreta;
+
+    public int getmQuestaoID(){
+        return mQuestaoID;
+    }
+
+    public void setmQuestaoID(int mQuestaoID){
+        this.mQuestaoID=mQuestaoID;
+    }
+
+    public boolean mRespostaCorreta() {
+        return mRespostaCorreta;
+    }
+
+    public void setmRespostaCorreta(boolean mRespostaCorreta) {
+        this.mRespostaCorreta = mRespostaCorreta;
+    }
+
+    public Question (int mQuestaoID, boolean mRespostaCorreta){
+        this.mQuestaoID=mQuestaoID;
+        this.mRespostaCorreta=mRespostaCorreta;
+    }
+}
--- app/src/main/java/br/com/gustech/android/geoquiz/QuizActivity.java
+++ app/src/main/java/br/com/gustech/android/geoquiz/QuizActivity.java
@@ -4,25 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button botaoTrue;
-    private Button botaoFalse;
+    private Button mBotaoTrue;
+    private Button mBotaoFalse;
+    private Button mBotaoNext;
 
+    private int mQuestaoCorrente =0;
 
+    private TextView mQuestoes;
 
+    private Question[] mBancoDeQuestoes = new Question[]{
+            new Question(R.string.questao1,true),
+            new Question(R.string.questao2,true),
+            new Question(R.string.questao3,false),
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        botaoTrue = (Button) findViewById(R.id.true_button);
-        botaoFalse = (Button) findViewById(R.id.false_button);
+        mBotaoTrue = (Button) findViewById(R.id.true_button);
+        mBotaoFalse = (Button) findViewById(R.id.false_button);
+        mQuestoes = (TextView) findViewById(R.id.questoes);
+        mBotaoNext = (Button) findViewById(R.id.next_button);
 
-        botaoTrue.setOnClickListener(new View.OnClickListener() {
+        mBotaoTrue.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizActivity.this,
@@ -31,7 +42,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        botaoFalse.setOnClickListener(new View.OnClickListener() {
+        mBotaoFalse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(QuizActivity.this,
@@ -39,6 +50,21 @@ public class QuizActivity extends AppCompatActivity {
                         Toast.LENGTH_SHORT).show();
             }
         });
+
+        mBotaoNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mQuestaoCorrente = (mQuestaoCorrente +1) % mBancoDeQuestoes.length;
+                int idQuestion = mBancoDeQuestoes[mQuestaoCorrente].getmQuestaoID();
+                mQuestoes.setText(idQuestion);
+            }
+        });
+
+
+        int question = mBancoDeQuestoes[mQuestaoCorrente].getmQuestaoID();
+        mQuestoes.setText(question);
+
+
 
     }
 }
